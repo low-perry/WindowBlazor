dced90657099a449123bc62f34a239c43f4826c2
low-perry <dariohaxhiraj@hotmail.com>
2023-12-20 18:27:17 +0100

    Addding The client and the server apps. Just the default blazor projects.

--- WindowBlazor_Server/Program.cs
+++ WindowBlazor_Server/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using WindowBlazor.Server.Data;
+using WindowBlazor_Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
